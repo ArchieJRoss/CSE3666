c43594a1499098f38a19199ce0a68fccd78b0f13
ArchieJRoss <130590196+ArchieJRoss@users.noreply.github.com>
2023-04-12 13:37:16 -0400

    Add files via upload

---
+++ Labs/LAB6/Archie needs to do his work.py
@@ -0,0 +1,9 @@
+Archie = "Archie needs to do his work."
+def Alex():
+    print(Archie)
+Alex()
+
+print("Archie needs to do his work.")
+while True:
+    print("Archie needs to do his work.")
+    
